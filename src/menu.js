a8365f64cdb362a692e23a69fbfdd43e05e083b8
Elgareh Mouataz <mouatazelgareh044@gmail.com>
2025-11-24 17:45:38 +0100

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,29 +1,43 @@
 import React from "react";
 import bike from "./images/MenuBike.png";
 
-function MenuElement() {
+
+const products = [
+  { image:bike, descrption: "Road Bike" },
+  { image:bike, descrption: "Road Bike" },
+  { image:bike, descrption: "Road Bike" },
+  { image:bike, descrption: "Road Bike" }
+];
+
+function MenuElement(props) {
   return (
     <React.Fragment>
-    <div className="menu-element col d-flex flex-column align-items-center">
-      <div className="display mb-2">
-        <img className="bike" src={bike} alt="bike" />
+      <div className="menu-element col d-flex flex-column align-items-center">
+        <div className="display mb-2">
+          <img className="bike" src={props.image} alt="bike" />
+        </div>
+        <div className="text-center font-weight-bold">
+          {props.description}
+        </div>
       </div>
-      <div className="text-center font-weight-bold">Road bike</div>
-    </div>
     </React.Fragment>
   );
 }
 
 function Menu() {
-  return (<React.Fragment>
-    <div className="menu">
-      <div className="row no-wrap">
-        {MenuElement()}
-        {MenuElement()}
-        {MenuElement()}
-        {MenuElement()}
+  return (
+    <React.Fragment>
+      <div className="menu">
+        <div className="row no-wrap">
+          {products.map((p, i) => (
+            <MenuElement
+              key={i}
+              image={p.image}
+              description={p.descrption}
+            />
+          ))}
+        </div>
       </div>
-    </div>
     </React.Fragment>
   );
 }
