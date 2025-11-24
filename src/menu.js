fddcb5261e414d9cc4763fbb979c5ae240308866
Elgareh Mouataz <discordcappin101@gmail.com>
2025-11-24 15:05:45 +0100

    addition element menu
    addition de menu

--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,18 @@
-
+import React from "react";
+import menuitem from './images/menu-item.png'
 function Menu(){
 
 
-    return (<h1>Menu</h1>)
+    return (<React.Fragment>
+            <div className='menu'>
+                <div className='row'>
+                <img src={menuitem} alt='menu item'></img>
+                <img src={menuitem} alt='menu item'></img>
+                <img src={menuitem} alt='menu item'></img>
+                <img src={menuitem} alt='menu item'></img>
+                </div>
+            </div>
+            </React.Fragment>)
 }
 
 export default Menu;
