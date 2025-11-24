555aa601f9d53513f6fe652c19799ab4fd56690f
Elgareh Mouataz <discordcappin101@gmail.com>
2025-11-24 15:43:17 +0100

    refaire element menu

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,18 @@
 import React from "react";
-import menuitem from './images/menu-item.png'
+import bike from "./images/MenuBike.png"
 function Menu(){
-
-
+    //var elements=['']
     return (<React.Fragment>
             <div className='menu p-4'>
                 <div className='row'>
-                <img className='MenuImage' src={menuitem} alt='menu item'></img>
-                <img className='MenuImage' src={menuitem} alt='menu item'></img>
-                <img className='MenuImage' src={menuitem} alt='menu item'></img>
-                <img className='MenuImage' src={menuitem} alt='menu item'></img>
+                    <div clasName='column '>
+                    <div className='display'>
+                        <img className='bike' src={bike} alt='bike'></img>
+                    </div>
+                    <div className='text-center font-weight-bold'>
+                    Road bike
+                    </div>
+                    </div>
                 </div>
             </div>
             </React.Fragment>)
