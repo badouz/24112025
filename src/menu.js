2b2303916ed92d3760d3a38f40371b55f39daa36
Elgareh Mouataz <mouatazelgareh044@gmail.com>
2025-11-24 16:57:32 +0100

    modification de menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,21 +1,31 @@
 import React from "react";
-import bike from "./images/MenuBike.png"
-function Menu(){
-    //var elements=['']
-    return (<React.Fragment>
-            <div className='menu p-4'>
-                <div className='row'>
-                    <div clasName='column '>
-                    <div className='display'>
-                        <img className='bike' src={bike} alt='bike'></img>
-                    </div>
-                    <div className='text-center font-weight-bold'>
-                    Road bike
-                    </div>
-                    </div>
-                </div>
-            </div>
-            </React.Fragment>)
+import bike from "./images/MenuBike.png";
+
+function MenuElement() {
+  return (
+    <React.Fragment>
+    <div className="menu-element col d-flex flex-column align-items-center">
+      <div className="display mb-2">
+        <img className="bike" src={bike} alt="bike" />
+      </div>
+      <div className="text-center font-weight-bold">Road bike</div>
+    </div>
+    </React.Fragment>
+  );
+}
+
+function Menu() {
+  return (<React.Fragment>
+    <div className="menu">
+      <div className="row no-wrap ">
+        {MenuElement()}
+        {MenuElement()}
+        {MenuElement()}
+        {MenuElement()}
+      </div>
+    </div>
+    </React.Fragment>
+  );
 }
 
 export default Menu;
